0abcb739cd69135aa4889d9b7fac2777c785953a
SXD <sxd4business@qq.com>
2019-02-25 16:06:48 +0800

    spring boot 對接ES 完成CRUD 详细地址：https://www.cnblogs.com/sxdcgaq8080/p/10411423.html

---
+++ src/main/java/com/sxd/swapping/esDao/BuilderDao.java
@@ -0,0 +1,8 @@
+package com.sxd.swapping.esDao;
+
+import com.sxd.swapping.domain.Builder;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface BuilderDao extends ElasticsearchRepository<Builder,Long>{
+
+}
