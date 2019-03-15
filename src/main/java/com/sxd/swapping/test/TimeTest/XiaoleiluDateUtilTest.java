546661115ca861b3bcdd00b89dbba1ea5333f408
SXD <sxd4business@qq.com>
2019-03-15 10:21:36 +0800

    添加 111

---
+++ src/main/java/com/sxd/swapping/test/TimeTest/XiaoleiluDateUtilTest.java
@@ -0,0 +1,24 @@
+package com.sxd.swapping.test.TimeTest;
+
+import com.xiaoleilu.hutool.date.DateUtil;
+
+import java.util.Date;
+
+public class XiaoleiluDateUtilTest {
+
+//    public static void main(String[] args) {
+//        Date date = new Date();
+//        System.out.println(date);
+//        Date date2 = DateUtil.offsetMonth(date,-3);
+//        System.out.println(date2);
+//        Date date3 = DateUtil.offsetDay(date2,1);
+//        System.out.println(date3);
+//        Date date4 = DateUtil.offsetDay(date2,-3);
+//        System.out.println(date4);
+//        System.out.println(DateUtil.betweenMonth(date,date2 , true));
+//        System.out.println(DateUtil.betweenMonth(date,date3 , true));
+//        System.out.println(DateUtil.betweenMonth(date,date3 , false));
+//        System.out.println(DateUtil.betweenMonth(date,date4 , true));
+//        System.out.println(DateUtil.betweenMonth(date,date4 , false));
+//    }
+}
