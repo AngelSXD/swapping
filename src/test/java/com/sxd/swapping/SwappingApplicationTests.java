267596444e46274889798e0526dad26d4fc88d81
SXD <sxd4business@qq.com>
2020-10-12 19:22:36 +0800

    1.本地没有es环境，因此测试入口 仅加载某个Bean，否则会默认加载@SpringBoot注解下要加载的所有Bean

--- src/test/java/com/sxd/swapping/SwappingApplicationTests.java
+++ src/test/java/com/sxd/swapping/SwappingApplicationTests.java
@@ -1,6 +1,8 @@
 package com.sxd.swapping;
 
 import com.sxd.swapping.controller.FirstController;
+import com.sxd.swapping.service.BaseService;
+import com.sxd.swapping.service.impl.BaseServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//编写测试类的时候，如果明确这个测试类会用到哪几个 Bean，则可以在 classes 属性处指定，之后启动测试类的时候，就只会加载需要的 Bean 到上下文中，从而加快启动速度
+//否则 只使用@SpringBootTest注解即可
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes={BaseServiceImpl.class,FirstController.class})
 public class SwappingApplicationTests {
 
     @Autowired
